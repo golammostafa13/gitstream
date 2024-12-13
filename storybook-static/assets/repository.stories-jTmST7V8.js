d830953234fed641a3193e98d7f6d1a6fd7c3c91
golam.mostafa <golam.mostafa@spctrum-bd.com>
2024-12-13 17:21:08 +0600

    repo to build storybook

--- storybook-static/assets/repository.stories-jTmST7V8.js
+++
@@ -1,3 +0,0 @@
-import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const i=()=>e.jsx("div",{children:e.jsx("button",{type:"button",children:"Click me"})}),n=i;i.__docgenInfo={description:"",methods:[],displayName:"Repository"};const c={title:"gitstream/components/Repositories",component:n},o={args:{}};var t,s,r;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {}
-}`,...(r=(s=o.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const m=["Default"];export{o as Default,m as __namedExportsOrder,c as default};
